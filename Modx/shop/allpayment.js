2845371756bd216a1292327992dc62d2a300e1de
MDzRxNc <delonleo4@gmail.com>
2021-12-15 16:49:11 +0700

    MDzRxNc

---
+++ Modx/shop/allpayment.js
@@ -0,0 +1,24 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY DELONGANSS
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0895 2376 7913
+2. OVO
+	ㅁ 0895 2376 7913
+3. DANA
+	ㅁ 0895 2376 7913
+	
+Semuanya atas nama RI****DI
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ Modx/shop/donasi.js
@@ -0,0 +1,20 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 089523767913
+2. OVO
+	ㅁ 089523767913
+3. DANA
+	ㅁ 089523767913
+	
+Semuanya atas nama MoDxVip
+`
+	}
+
+exports.donasibot = donasibot
+ 
